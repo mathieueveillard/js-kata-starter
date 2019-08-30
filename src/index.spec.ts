c444bf0c2485370da210c23224f8b9c753f4c57e
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2019-08-30 23:38:01 +0200

    Stack upgrade

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,7 @@
-describe("Test of the test framework", function() {
-  it.skip("Should fail", function() {
-    expect(0).toEqual(1);
-  });
+import { doTheThing } from ".";
 
+describe("Test of the test framework", function() {
   it("Should pass", function() {
-    expect(0).toEqual(0);
+    expect(doTheThing()).toEqual(true);
   });
 });
