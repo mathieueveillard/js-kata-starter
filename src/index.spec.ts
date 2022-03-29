f7bc9a09df26af54074d34a35fedef0a8afe1858
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-03-29 12:52:17 +0200

    Add jest-extended

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,11 @@
+// @ts-ignore see https://github.com/jest-community/jest-extended#setup
+import * as matchers from "jest-extended";
+expect.extend(matchers);
+
 it("That's a test!", function () {
   expect(1 + 1).toEqual(2);
+});
+
+it("jest-extended is included", function () {
+  expect([1, 0]).toIncludeSameMembers([0, 1]);
 });
