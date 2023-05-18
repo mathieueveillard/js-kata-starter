f074f1e2fc9a4c9b15dbd8704073d18c77cd0df6
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-05-18 04:12:45 +0200

    Add fast-check (Property-based testing)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,32 @@
 // @ts-ignore see https://github.com/jest-community/jest-extended#setup
 import * as matchers from "jest-extended";
+import fc from "fast-check";
 
 expect.extend(matchers);
 
-test("That's a test!", () => {
+test("A simple test (Jest)", () => {
   expect(1 + 1).toEqual(2);
 });
 
-test("jest-extended is included", () => {
+test("Additional matchers (jest-extended)", () => {
   expect([1, 0]).toIncludeSameMembers([0, 1]);
+});
+
+test("Property-based testing (fast-check)", () => {
+  type Boundaries = {
+    min: number;
+    max: number;
+  };
+
+  const minmax =
+    ({ min, max }: Boundaries) =>
+    (n: number): number =>
+      Math.min(max, Math.max(min, n));
+
+  fc.assert(
+    fc.property(fc.integer(), (n): boolean => {
+      const result = minmax({ min: 1, max: 10 })(n);
+      return 1 <= result && result <= 10;
+    })
+  );
 });
